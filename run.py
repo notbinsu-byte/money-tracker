8fe92a347bbcd382d948e2464561525f09aadbd2
not.binsu <not.binsu@gmail.com>
2026-03-07 23:05:48 -0600

    updated for access across platforms

--- run.py
+++ run.py
@@ -3,9 +3,10 @@ import os
 
 if __name__ == "__main__":
     debug = os.getenv("DEBUG", "false").lower() == "true"
+    host = os.getenv("HOST", "127.0.0.1")
     uvicorn.run(
         "app.main:app",
-        host="127.0.0.1",
+        host=host,
         port=8000,
         reload=debug,
     )
--- start.bat
+++ start.bat
@@ -1,3 +1,4 @@
 @echo off
 call .venv\Scripts\activate
+set HOST=100.84.150.81
 python run.py
